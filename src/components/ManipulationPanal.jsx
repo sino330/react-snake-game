0ded7f5225e9456d229e24aa2db682f9c780141e
sino330 <narutokozo330@gmail.com>
2021-10-26 15:19:43 +0900

    2021/10/26

--- src/components/ManipulationPanal.jsx
+++ src/components/ManipulationPanal.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faArrowLeft,faArrowUp,faArrowDown,faArrowRight} from "@fortawesome/free-solid-svg-icons"
+import { Direction } from "../constants";
 
 const ManipulationPanel = ({ onChange }) => {
-  const onUp = () => onChange("up");
-  const onRight = () => onChange("right");
-  const onLeft = () => onChange("left");
-  const onDown = () => onChange("down");
+  const onUp = () => onChange(Direction.up);
+  const onRight = () => onChange(Direction.right);
+  const onLeft = () => onChange(Direction.left);
+  const onDown = () => onChange(Direction.down);
 
   return (
     <div className="manipulation-panel">
